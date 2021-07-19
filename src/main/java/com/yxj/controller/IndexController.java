95e79222f6b13c686d072876a7eb679c2ee08028
yxj <17804331763@163.com>
2021-07-19 19:01:30 +0800

    add index.html

--- src/main/java/com/yxj/controller/IndexController.java
+++ src/main/java/com/yxj/controller/IndexController.java
@@ -1,18 +1,18 @@
 package com.yxj.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author LYJ
  * @create 2021-07-19 18:08
  */
 
-@RestController
-public class HelloController {
+@Controller
+public class IndexController {
 
     @RequestMapping("/")
     public String hello(){
-        return "Hello World!";
+        return "index";
     }
 }
